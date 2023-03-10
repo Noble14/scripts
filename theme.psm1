e00f088a247dc6a5f079eec35ab92ca37c80bc37
Noble14 <nemesz2000@gmail.com>
2023-03-10 11:29:43 +0100

    fix changes not being applied instantly

--- theme.psm1
+++ theme.psm1
@@ -11,5 +11,34 @@ function Set-Theme {
     }
     Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value $val
     Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value $val
-    
+
+    $explorers = get-explorerpaths;
+    Stop-Process -name explorer;
+    start-explorers $explorers;
+
+}
+
+function get-explorerpaths () {
+    $shell = New-Object -ComObject Shell.Application
+    $windows = $shell.Windows()
+    $explorerPaths = @()
+
+    foreach ($window in $windows) {
+        if ($null -ne $window.Document.Folder.Self.Path) {
+            $explorerPaths += $window.Document.Folder.Self.Path
+        }
+    }
+
+    return $explorerPaths
+}
+function start-explorers () {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string[]]
+        $paths
+    )
+    foreach ($path in $paths) {
+        explorer.exe $path
+    }
 }
